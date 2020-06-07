33b1552dd2a4335123fcc9dd73a6fa4c7e5eb3ac
Steven <tshao.s@gmail.com>
2020-06-07 01:18:52 -0400

    redo in the right order lol

--- editor/PxtoneActionSynchronizer.cpp
+++ editor/PxtoneActionSynchronizer.cpp
@@ -107,39 +107,61 @@ void PxtoneActionSynchronizer::applyRemoteAction(
       break;
     case RemoteAction::Type::UNDO:
     case RemoteAction::Type::REDO:
-      qDebug() << "Applying undo / redo";
+      qDebug() << "Applying undo";
+      if (m_log.size() == 0) {
+        qDebug() << "No actions in the log. Doing nothing.";
+        break;
+      }
+
+      auto target = m_log.rend();
+      // If we're redoing, we're eventually going to want to find the first
+      // undone item by this user.
+      if (action.type == RemoteAction::REDO) {
+        for (auto it = m_log.begin(); it != m_log.end(); ++it)
+          if (it->state == LoggedAction::UNDONE && it->uid == uid) {
+            target = std::reverse_iterator(it + 1);
+            break;
+          }
+      } else if (action.type == RemoteAction::UNDO) {
+        // Likewise, if undoing, find the last done item.
+        for (auto it = m_log.rbegin(); it != m_log.rend(); ++it)
+          if (it->state == LoggedAction::DONE && it->uid == uid) {
+            target = it;
+            break;
+          }
+      }
+      if (target == m_log.rend()) {
+        qDebug()
+            << "User has no more actions to undo / redo in this direction.";
+        break;
+      }
+
       for (auto uncommitted = m_uncommitted.rbegin();
            uncommitted != m_uncommitted.rend(); ++uncommitted) {
         *uncommitted =
             apply_actions_and_get_undo(*uncommitted, m_pxtn, &widthChanged);
       }
 
-      // Go back to last {undone,done} action by user, temporarily undoing done
-      // actions by other users. Flip. Then redo the done actions by other
-      // users.
+      // Go back to the target action by user, temporarily undoing
+      // done actions by other users. Flip. Then redo the done actions by
+      // other users.
       {
-        auto flip_from =
-            (action.type == RemoteAction::UNDO ? LoggedAction::DONE
-                                               : LoggedAction::UNDONE);
-        auto flip_to = (action.type == RemoteAction::UNDO ? LoggedAction::UNDONE
-                                                          : LoggedAction::DONE);
-
-        // qDebug() << "m_log size" << m_log.size();
         std::list<LoggedAction *> temporarily_undone;
-        for (auto it = m_log.rbegin(); it != m_log.rend(); ++it) {
-          if (it->state == flip_from && it->uid == uid) {
-            // qDebug() << "Flipped " << it->uid << it->idx;
-            it->reverse =
-                apply_actions_and_get_undo(it->reverse, m_pxtn, &widthChanged);
-            it->state = flip_to;
-            break;
-          } else if (it->state == LoggedAction::UndoState::DONE) {
+        auto it = m_log.rbegin();
+        while (it != target) {
+          if (it->state == LoggedAction::UndoState::DONE) {
             qDebug() << "Temporarily undoing " << it->uid << it->idx;
             it->reverse =
                 apply_actions_and_get_undo(it->reverse, m_pxtn, &widthChanged);
             temporarily_undone.push_front(&(*it));
           }
+          ++it;
         }
+        it->reverse =
+            apply_actions_and_get_undo(it->reverse, m_pxtn, &widthChanged);
+        it->state = (it->state == LoggedAction::UNDONE ? LoggedAction::DONE
+                                                       : LoggedAction::UNDONE);
+        ;
         for (LoggedAction *it : temporarily_undone)
           it->reverse =
               apply_actions_and_get_undo(it->reverse, m_pxtn, &widthChanged);
