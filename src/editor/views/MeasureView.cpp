6fcfa6dec539d6f6e80a24899b1556ff892e119d
Steven <tshao.s@gmail.com>
2021-03-02 22:44:08 -0500

    scale error in midi input measure ribbon

--- src/editor/views/MeasureView.cpp
+++ src/editor/views/MeasureView.cpp
@@ -156,7 +156,7 @@ void drawOngoingAction(const EditState &state, QPainter &painter, int height,
     const Input::State::On &v = state.m_input_state.value();
 
     for (const Interval &interval : v.clock_ints(now, master))
-      drawVelAction(interval, v.on.vel, 255);
+      drawVelAction(interval / state.scale.clockPerPx, v.on.vel, 255);
   }
 }
 
