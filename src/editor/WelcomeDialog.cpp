61a48bd29d97984f02b047bbed87072c164f1b80
Steven <tshao.s@gmail.com>
2021-07-24 20:11:19 -0400

    remove comment b/c I think the line of code is a fine thing to do by itself

--- src/editor/WelcomeDialog.cpp
+++ src/editor/WelcomeDialog.cpp
@@ -8,11 +8,7 @@ WelcomeDialog::WelcomeDialog(QWidget *parent)
   ui->setupUi(this);
   ui->labelVersion->setText(qApp->applicationName() + " " +
                             qApp->applicationVersion());
-  ui->showAgainCheck->setChecked(
-      Settings::ShowLandingPage::get());  // Shouldn't be necessary but if it is
-                                          // somehow false i want the checkbox
-                                          // to be right
-
+  ui->showAgainCheck->setChecked(Settings::ShowLandingPage::get());
   QString connectToolTip = tr("Connect to an existing server.");
   QString newToolTip = tr("Create a new project without hosting.");
   QString openToolTip = tr("Open an existing project for offline use.");
