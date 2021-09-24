a0bf5d171c0b9cfe0b1f96e84201da43c70bba39
Steven <tshao.s@gmail.com>
2021-09-23 23:13:57 -0400

    shorten

--- src/editor/sidemenu/IconHelper.cpp
+++ src/editor/sidemenu/IconHelper.cpp
@@ -4,23 +4,24 @@
 #include <QPainter>
 #include <QPalette>
 
-void recolor(QPainter *painter, const QPixmap &src, QColor color) {
+void recolor(QPixmap &src, QColor color) {
   QImage srcImage = src.toImage();
+
   const auto format = srcImage.hasAlphaChannel()
                           ? QImage::Format_ARGB32_Premultiplied
                           : QImage::Format_RGB32;
   srcImage = std::move(srcImage).convertToFormat(format);
 
   QRect rect(0, 0, src.width(), src.height());
+
+  // create colorized image
   QImage destImage = QImage(rect.size(), srcImage.format());
   destImage.setDevicePixelRatio(src.devicePixelRatioF());
-
-  // do colorizing
-  QPainter destPainter(&destImage);
-  destPainter.fillRect(rect, color);
-  destPainter.end();
+  QPainter(&destImage).fillRect(rect, color);
   destImage.setAlphaChannel(srcImage.convertToFormat(QImage::Format_Alpha8));
-  painter->drawImage(QPoint(0, 0), destImage);
+
+  // apply over original
+  QPainter(&src).drawImage(QPoint(0, 0), destImage);
 }
 // Adapted from
 // https://code.woboq.org/qt5/qtbase/src/widgets/effects/qpixmapfilter.cpp.html#1089
@@ -28,7 +29,6 @@ void recolor(QPainter *painter, const QPixmap &src, QColor color) {
 
 QIcon getIcon(QString name) {
   QPixmap px = QIcon(":/icons/neutral/" + name).pixmap(128, 128);
-  QPainter p(&px);
-  recolor(&p, px, qApp->palette().color(QPalette::ButtonText));
+  recolor(px, qApp->palette().color(QPalette::ButtonText));
   return QIcon(px);
 }
