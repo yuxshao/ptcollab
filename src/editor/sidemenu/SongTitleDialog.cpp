1c581d51a76f6ae7a070d28ade9bfd0f53d0552a
Ewan Green <ewangreen95@gmail.com>
2023-02-28 22:06:21 -0700

    :(

--- src/editor/sidemenu/SongTitleDialog.cpp
+++ src/editor/sidemenu/SongTitleDialog.cpp
@@ -13,7 +13,10 @@ SongTitleDialog::SongTitleDialog(const QString &title, const QString &comment,
 
 QString SongTitleDialog::title() { return ui->titleLineEdit->text(); }
 QString SongTitleDialog::comment() {
-  return ui->commentTextEdit->toPlainText().replace('\n', "\r\n");
+  return ui->commentTextEdit->toPlainText()
+      .replace('\n', "\r\n")
+      .replace("\r\r\n",
+               "\r\n");  // gross. i'll replace with something better later
 }
 
 SongTitleDialog::~SongTitleDialog() { delete ui; }
